2ef94864f12fdb53763e77807aaea5e0d0d6618e
falaeco <nic.prudhomme3@gmail.com>
2020-03-23 14:53:46 -0400

    added fonts and buttons. configured tailwind local

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {
+      'display' : ['Supply', 'Consolas','monospace'],
+      'body' : ['Object Sans', 'Avenir Next', 'Avenir', 'Century Gothic', 'Helvetica', 'Arial', 'sans-serif']
+    }
+  },
+  variants: {},
+  plugins: [],
+}
