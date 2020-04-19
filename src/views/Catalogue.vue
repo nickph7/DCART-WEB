1b15d9489070d1f278dbc739c7046ac39b562d48
mgs095 <markus.specogna@gmail.com>
2020-04-19 11:02:13 -0400

    Update Catalogue.vue

--- src/views/Catalogue.vue
+++ src/views/Catalogue.vue
@@ -1,20 +1,9 @@
 <template>
   <main>
-    <Intro :pageTitle="page.title" />
-
-<div>
-  hello
-</div>
-    <!-- <div class="notes">
-      <article v-for="note in notes" :key="note.id" class="note">
-        <header class="note-header">
-          <router-link :to="'/' + note.id">
-            <h2>{{ note.content.title }}</h2>
-            <time>{{ note.content.date | format('day month year') }}</time>
-          </router-link>
-        </header>
-      </article>
-    </div> -->
+    <!-- <Intro :pageTitle="page.title" /> -->
+    <div class="issuu py-10 px-10">
+      hello
+    </div>
   </main>
 </template>
 
@@ -39,24 +28,8 @@ export default {
 </script>
 
 <style>
-/* .notes {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-  grid-gap: 1.5rem;
-  grid-auto-rows: 1fr;
+.issuu {
+  height: 100vh;
+  border-bottom: 1px dashed cyan;
 }
-.note {
-  border: 2px solid #000;
-}
-.note a {
-  display: block;
-  padding: 1rem;
-  line-height: 1.25em;
-}
-.note h2 {
-  font-size: 1rem;
-}
-.note time {
-  font-size: 0.75rem;
-} */
 </style>
